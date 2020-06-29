1e328c76fde83b4a1a8e88ee8eec15eaea8ae057
Hidetaka Okamoto <info@wp-kyoto.net>
2020-06-30 00:45:27 +0900

    test: add test code

--- test/nodejs-lambda-version.test.ts
+++ test/nodejs-lambda-version.test.ts
@@ -5,6 +5,11 @@ import { NodejsLambdaVersion } from '../lib/index';
 import { join } from 'path';
 
 describe('test', () => {
+  const expectedResources = [
+    'AWS::Lambda::Function',
+    'AWS::Lambda::Version',
+    'AWS::IAM::Role',
+  ];
   const app = new cdk.App();
   const stack = new cdk.Stack(app, 'TestStack');
   // WHEN
@@ -15,7 +20,21 @@ describe('test', () => {
     runtime: Runtime.NODEJS_12_X,
   });
   it('should match snapshot', () => {
-    console.log(SynthUtils.toCloudFormation(stack));
     expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
+  // THEN
+  it.each(expectedResources)('should have resource: %p', name => {
+    expectCDK(stack).to(haveResource(name));
+  });
+  it('should not create another resource', () => {
+    const cfn = SynthUtils.toCloudFormation(stack);
+    const unexpectedResources = Object.values(cfn.Resources).filter(
+      (resource: any) => {
+        if (!resource) return false;
+        if (expectedResources.includes(resource.Type)) return false;
+        return true;
+      }
+    );
+    expect(unexpectedResources.length).toEqual(0);
   });
 });
